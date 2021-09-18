3cbedbeb6c68c0b69e0dc57cf2866bfceed21395
Hayk04 <yeritsyan04@gmail.com>
2021-09-18 16:16:42 +0400

    Add posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'posts', to: "posts#index", as: 'posts'
+  get 'posts/:id', to: "posts#show", as: 'post'
   root "home#index"
   namespace :admin do 
     get '/', to: 'home#index'
+    resources :posts
   end  
 end
 
